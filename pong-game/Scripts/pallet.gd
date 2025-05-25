6d5a2862574dde560541b5ac65eadcbf5cc13339
Mister_yay <gettingbetter.y@gmail.com>
2025-05-25 15:17:53 -0600

    Two players control
    Now there are controls for both players when playing the game on player vs player. Note: Computer vs Player doesnt work yet

--- pong-game/Scripts/pallet.gd
+++ pong-game/Scripts/pallet.gd
@@ -8,13 +8,3 @@ extends CharacterBody2D
 
 func _ready() -> void:
 	Input.MouseMode.MOUSE_MODE_CAPTURED
-
-func _physics_process(delta: float) -> void:
-	velocity = Vector2.ZERO
-	
-	if Input.is_action_pressed("Down1"):
-		velocity.y += speed
-	if Input.is_action_pressed("Up1"):
-		velocity.y -= speed
-		
-	move_and_slide()
--- pong-game/Scripts/user_input.gd
+++ pong-game/Scripts/user_input.gd
@@ -1,1 +1,29 @@
 extends Node
+
+var speed = 300.0 
+var player1
+var player2
+
+func _ready() -> void:
+	var pallets = get_parent().get_pallets()
+	player1 = pallets[0]
+	player2 = pallets[1]
+	
+func _physics_process(delta: float) -> void:
+	player1.velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("Down1"):
+		player1.velocity.y += speed
+	if Input.is_action_pressed("Up1"):
+		player1.velocity.y -= speed
+		
+	player1.move_and_slide()
+	
+	player2.velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("Down2"):
+		player2.velocity.y += speed
+	if Input.is_action_pressed("Up2"):
+		player2.velocity.y -= speed
+		
+	player2.move_and_slide()
