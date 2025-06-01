fbf7fc353838ff6887d347c513ebfa72153c48bb
Mister_yay <gettingbetter.y@gmail.com>
2025-06-01 14:16:20 -0600

    Basic HUD functionality
    Options work now

--- pong-game/Scripts/hud.gd
+++ pong-game/Scripts/hud.gd
@@ -1,1 +1,24 @@
 extends Control
+
+@onready var first_page: VBoxContainer = $FirstPage
+@onready var second_page: VBoxContainer = $SecondPage
+
+
+func _ready() -> void:
+	second_page.hide()
+	
+
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_play_button_pressed() -> void:
+	first_page.hide()
+	second_page.show()
+
+
+func _on_back_button_pressed() -> void:
+	second_page.hide()
+	first_page.show()
